7fcdebe933a0d8998d9dda9e69f4db96c2ebd631
Nina Dokukina <ninamoiseenkova@gmail.com>
2020-11-09 20:14:58 +0700

    add to cart and remove (Vue main)

--- Project/js/product.js
+++ Project/js/product.js
@@ -3,21 +3,64 @@ const API = 'https://raw.githubusercontent.com/Shustrushka/static/master';
 const app = new Vue({
     el: '#app',
     data: {
+        userSearch: '',
+        showCart: false,
+        cartUrl: '/getBasket.json',
+        imgCart: 'https://via.placeholder.com/72x85',
+        cartItems: [],
         catalogUrl: '/catalogData.json',
-        products: []
+        products: [],
+        filtered: []
     },
     methods: {
         getJson(url){
                      return fetch(url)
                              .then(result => result.json())
                              .catch(error => console.log(error));
+        },
+        addProduct(product){
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result){
+                        let find = this.cartItems.find(el => el.id === product.id);
+                        if(find){
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({quantity: 1}, product);
+                            this.cartItems.push(prod);
+                        }
+                    }
+                })
+        },
+        remove(product){
+            this.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if(data.result){
+                        if(product.quantity > 1){
+                            product.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(product), 1);
+                        }
+                    }
+                })
+        },
+        filter(){
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.title));
         }
     },
     mounted() {
-        this.getJson(`${API+catalogUrl}`)
+        this.getJson(`${API+this.cartUrl}`)
+            .then(data => {
+                for (let el of data.contents) {
+                    this.cartItems.push(el);
+                }
+            });
+        this.getJson(`${API+this.catalogUrl}`)
             .then(data => {
                 for (let el of data) {
                     this.products.push(el);
+                    this.filtered.push(el);
                 }
             });
     }
